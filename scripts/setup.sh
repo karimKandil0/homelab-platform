0d7f8af8077be4f7a350e57042c9a58aaa5fda27
karimKandil0 <kimo989yt@gmail.com>
2026-03-11 20:54:06 +0200

    chore: fixed karimkandil variable for containers (setup.sh)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -166,7 +166,7 @@ echo "Setting container permissions..."
 sudo chown -R 1000:1000 data/vaultwarden || true
 sudo chown -R 1000:1000 data/gitea || true
 sudo chown -R 472:472 data/grafana || true
-sudo chown -R $(id -u):$(id -g) data
+sudo chown -R $(id -u):$(id -g) data || true
 
 echo ""
 echo "You may need to log out and back in for Docker permissions"
