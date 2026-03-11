3adbb81b1cf215a3665f21b35c5cd372dea51c39
karimKandil0 <kimo989yt@gmail.com>
2026-03-11 20:22:08 +0200

    chore: fixed syntax issue in setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -63,6 +63,7 @@ case "$DISTRO" in
     fedora)
         $INSTALL_DOCKER && sudo dnf install -y docker docker-compose-plugin
 	$INSTALL_GETTEXT && sudo dnf install -y gettext
+	;;
     *)
         echo "Unsupported distro: $DISTRO"
 	exit 1
