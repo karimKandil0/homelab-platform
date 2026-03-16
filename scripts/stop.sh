24c97191ff690350ba60f45e0d82eb791d95d825
karimKandil0 <kimo989yt@gmail.com>
2026-03-16 22:33:33 +0200

    chore: updated stop.sh to build and use profiles array

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 set -e
+source .env
+
+# Make sure .env is loaded
+
+if [[ ! -f ".env" ]]; then
+    echo "Error: .env file not found."
+    echo "Run ./scripts/setup.sh first."
+    exit 1
+fi
 
 # Detect Docker Compose command
+
 if command -v docker-compose >/dev/null 2>&1; then
 	COMPOSE=(docker-compose)
 elif docker compose version >/dev/null 2>&1; then
@@ -12,9 +22,18 @@ else
     exit 1
 fi
 
+# Detect active profiles
+
+PROFILES=()
+
+[ "$ENABLE_VAULTWARDEN" = "Y" ] && PROFILES+=(--profile vaultwarden)
+[ "$ENABLE_GITEA" = "Y" ] && PROFILES+=(--profile gitea)
+[ "$ENABLE_GRAFANA" = "Y" ] && PROFILES+=(--profile grafana)
+
 echo "Stopping homelab platform..."
 
 "${COMPOSE[@]}" \
+  "${PROFILES[@]}" \
   --env-file .env \
   -f compose/stack.yml \
   down --remove-orphans
